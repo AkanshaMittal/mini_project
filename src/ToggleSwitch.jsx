cbdb19194faefb75fd521fcdeaea5fe3cd138fcb
AkanshaMittal <akanshamittal210@gmail.com>
2025-03-30 13:09:22 +0530

    adding images

--- src/ToggleSwitch.jsx
+++ src/ToggleSwitch.jsx
@@ -1,23 +1,26 @@
-import { useState } from "react"
+import { useState } from "react";
 import "./ToggleSwitch.css";
-export const ToggleSwitch = () => {
 
-    const [isopen, Setopen] = useState(false)
-    const handleClick = () => {
-        Setopen(!isopen)
-    }
-    return (
-        <div
-        className="container"
-        style={{ backgroundColor: isopen ? "green" : "red" }}
-      >
-        <div className="toggle-switch" onClick={handleClick}>
-        <div className={`switch ${isopen ? "ON" : "OFF"}`}>
-        {isopen ? "ON" : "OFF"}
+export const ToggleSwitch = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  const styling = { width: "187px", height: "182px", marginLeft: "200px" };
+  const condition=isOpen ? "ON" : "OFF"
+
+  const handleClick = () => {
+    setOpen(!isOpen);
+  };
+
+  return (
+    <div className="container" style={{ backgroundColor: isOpen ? "#FFED29" : "white" }}>
+      <div className="toggle-switch" onClick={handleClick}>
+        <div></div>
+        <div className={`switch ${condition}`}>
+          {condition}
           <span className="switch-state"></span>
         </div>
-
-        </div>
-        </div>
-    )
-}
+        <img src={isOpen ? "ONButton.jpg" : "OFFButton.jpg"} style={styling} alt="Toggle Button" />
+      </div>
+    </div>
+  );
+};
